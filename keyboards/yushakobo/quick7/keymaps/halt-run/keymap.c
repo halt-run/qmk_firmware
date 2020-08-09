5ededf64dd3c626a355a571852c224d3ae3fb99f
Henrik Johansson <hkj@halt.run>
2020-08-09 20:28:58 +0900

    Updated keymap file with new settings for the Halt Run keys.

--- keyboards/yushakobo/quick7/keymaps/halt-run/keymap.c
+++ keyboards/yushakobo/quick7/keymaps/halt-run/keymap.c
@@ -18,55 +18,23 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
-    _FUNC1
-};
-
-// Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes {
-    YUSHAURL = SAFE_RANGE
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        KC_MUTE,   MO(_FUNC1), RGB_MOD,
-        S(KC_TAB), KC_UP,      KC_TAB,
-        KC_LEFT,   KC_DOWN,    KC_RGHT
-    ),
-    [_FUNC1] = LAYOUT(
-        RESET,   KC_TRNS, RGB_TOG,
-        KC_HOME, KC_VOLU, KC_END,
-        KC_MPRV, KC_VOLD, KC_MNXT
+        LALT(KC_TAB),   KC_MUTE, RGB_MOD,
+        KC_MRWD,   KC_MPLY,    KC_MFFD,
+        KC_MPRV,   KC_MSTP,    KC_MNXT
     )
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case YUSHAURL:
-            if (record->event.pressed) {
-                // when keycode QMKURL is pressed
-                SEND_STRING("https://yushakobo.jp/\n");
-            } else {
-                // when keycode QMKURL is released
-            }
-            break;
-    }
-    return true;
-}
-
 void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { // Left encoder
+      if (index == 1) { // Right encoder
         if (clockwise) {
             tap_code(KC_VOLU);
         } else {
             tap_code(KC_VOLD);
-        }
-    }
-    else if (index == 1) { // Right encoder
-        if (clockwise) {
-            rgblight_decrease_hue_noeeprom();
-        } else {
-            rgblight_increase_hue_noeeprom();
         }
     }
 }
